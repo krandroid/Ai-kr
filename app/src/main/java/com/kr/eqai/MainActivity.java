832f93d244099441c37045aebc9df30ad7f75010
krandroid <kr.xperiasp@gmail.com>
2026-05-09 22:57:18 +0700

    Add minimal Android project

---
+++ app/src/main/java/com/kr/eqai/MainActivity.java
@@ -0,0 +1,1 @@
+package com.kr.eqai; import android.app.Activity; import android.os.Bundle; public class MainActivity extends Activity { protected void onCreate(Bundle b) { super.onCreate(b); setContentView(R.layout.activity_main); }}
